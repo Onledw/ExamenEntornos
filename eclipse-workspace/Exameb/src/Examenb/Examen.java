7c6edcd74f9dbf25d7aa535a02686e3ba96b2a41
CrisU <cjcris076@gmail.com>
2023-12-05 18:07:58 +0100

    MenuOpciones

---
+++ eclipse-workspace/Exameb/src/Examenb/Examen.java
@@ -0,0 +1,44 @@
+package Examenb;
+
+import java.util.Scanner;
+
+public class Examen {
+	public static void main (String[]args) {
+		Scanner sc = new Scanner(System.in);
+
+		int opcion;
+		System.out.println("Menú de opciones:");
+		System.out.println("1. Área triángulo");
+		System.out.println("2. Área círculo");
+		System.out.println("3. Área del cono");
+		System.out.print("Seleccione una opción: ");
+
+		opcion = sc.nextInt();
+
+		switch (opcion) {
+		case 1:
+			System.out.print("Ingrese la base del triángulo: ");
+			double baseTriangulo = sc.nextDouble();
+			System.out.print("Ingrese la altura del triángulo: ");
+			double alturaTriangulo = sc.nextDouble();
+
+			break;
+		case 2:
+			System.out.print("Ingrese el radio del círculo: ");
+			double radioCirculo = sc.nextDouble();
+
+			break;
+		case 3:
+			System.out.print("Ingrese el radio de la base del cono: ");
+			double radioCono = sc.nextDouble();
+			System.out.print("Ingrese la altura del cono: ");
+			double alturaCono = sc.nextDouble();
+			break;
+		default:
+		System.out.println("Opción no válida. Inténtelo de nuevo.");
+		}
+ }
+
+}
+
+
